939d742b2b36e34885fbf23d1d9901a6f851f307
Matt <matthew.sampson@connect.qut.edu.au>
2019-10-01 14:17:02 +1000

    Updated Part 1 script

--- Part_1_Script.m
+++ Part_1_Script.m
@@ -80,6 +80,101 @@ for m = 1:length(s)
     end
 end
 
+%% Part 1 b) %%
+% 2D Representation
+% Parasite model function used
+
+% Defining Parameters
+k1 = 1;
+k2 = 2;
+k3 = 2; %linspace(0,50,10);
+k4 = 4;
+k5 = 3;
+T = 20;
+tspan = [0,T];
+X_1_0 = 1;
+X_2_0 = 1;
+y0 = [X_1_0, X_2_0];
+
+% Calling ODE45 to solve parasite function
+
+
+f = @(t,y) Parasite(t,y,k1,k2,k3,k4,k5);
+[t,y] = ode45(f,tspan,y0);
+figure()
+hold on
+plot(t,y);
+legend("Pred (Parasite)","Prey")
+
+%% Parameter Sweep %%
+
+n = 201;
+k3 = linspace(0,50,n);
+Tol = 1e-2;
+Parms_Para = zeros(1,n);
+Parms_Prey = zeros(1,n);
+
+for i = 1:n
+    f = @(t,y) Parasite(t,y,k1,k2,k3(i),k4,k5);
+    [t,y] = ode45(f,tspan,y0);
+    if ((y(end,1) < Tol) && y(end,2) > Tol) % Stores succesfull paramters for Para => 0
+        Parms_Para(i) = k3(i);
+    end
+    if (abs(y(end,2) - 2) < Tol)  % Stores succesfull paramters for Prey => 2
+      Parms_Prey(i) = k3(i);  
+    end
+end
+
+%% Plotting Successful Parameters k3 %%
+
+Parms_Para = Parms_Para(Parms_Para ~= 0); % Getting rid of the zero's
+Parms_Prey = Parms_Prey(Parms_Prey ~= 0); 
+
+figure;
+subplot(1,2,1)
+boxplot(Parms_Para','Labels',{'Parasites ==> 0'})
+title('Parameter Sweep Parasite Model')
+xlabel('System Dynamics')
+ylabel('Value of k3')
+subplot(1,2,2)
+boxplot(Parms_Prey','Labels',{'Prey ==> 2'})
+title('Parameter Sweep Parasite Model')
+xlabel('System Dynamics')
+ylabel('Value of k3')
+
+%% Part 1 c) %%
+% Paired parameter sweep k3 and k4
+
+% Defining sweep variables
+
+n = 51;
+k3 = linspace(0,50,n);
+k4 = linspace(0,50,n);
+Parms_Para_c = zeros(n*n,2);  % Storing succesful parameters in an array
+Parms_Prey_c = zeros(n*n,2);
+
+
+for j = 1:n
+    for i = 1:n
+        f = @(t,y) Parasite(t,y,k1,k2,k3(i),k4(j),k5);
+        [t,y] = ode45(f,tspan,y0);
+        if ((y(end,1) < Tol) && y(end,2) > Tol) % Stores succesfull paramters for Para => 0
+            Parms_Para_c(i*j,1) = k3(i);
+            Parms_Para_c(i*j,2) = k4(j);
+        end
+        if (abs(y(end,2) - 2) < Tol)  % Stores succesfull paramters for Prey => 2
+            Parms_Prey_c(i*j,1) = k3(i);
+            Parms_Prey_c(i*j,2) = k4(j);
+            
+        end
+    end
+end
+
+%% Part 1 d) %%
+
+
+
+
 
 
 
