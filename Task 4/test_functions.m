896d23e3c4eb62325cf626fa392d158cc15410fb
richorelaxo <34532974+richorelaxo@users.noreply.github.com>
2019-10-18 13:08:45 +1000

    Add files via upload

--- Task 4/test_functions.m
+++ Task 4/test_functions.m
@@ -56,15 +56,19 @@ imshow(col_map, 'InitialMagnification', 'Fit');
 %  myVideo.FrameRate = 5;
 %  open(myVideo)
 
-M = 200;
+M = 500;
 N = 200;
 pop_dens = 0.3;         %10, 20, 30, 40[%]
 food_growth = true;    % false = random, true = localised
-f_th = 0.05;            % (0, 0.1)
-p_ttl = 12;              % [0,15]
-new_food = 300;         % 100, 200, 300, 400
+f_th = 0.02;            % (0, 0.1)
+p_ttl = 10;              % [0,15]
+new_food = 400;         % 100, 200, 300, 400
 
 pop_cnts = zeros(1,2);
+quad_cnts1 = zeros(1,2);
+quad_cnts2 = zeros(1,2);
+quad_cnts3 = zeros(1,2);
+quad_cnts4 = zeros(1,2);
 
 % Init
 [map, para_array, food_array] = init_array(N, pop_dens, food_growth, p_ttl);
@@ -89,8 +93,16 @@ for i = 1:M
     % Food growth
     [map, food_array] = food_grows(map, food_array, new_food, food_growth);
     
-    % population counts
-    [pop_cnts(1+i, 1), pop_cnts(1+i,2)] = pop_cnt(map);
+    % population counts    
+        
+        % entire map
+        [pop_cnts(1+i, 1), pop_cnts(1+i,2)] = pop_cnt(map);
+
+        % quadrants
+        [quad_cnts1(1+i, 1), quad_cnts1(1+i,2)] = pop_cnt(map(1:N/2, 1:N/2));
+        [quad_cnts2(1+i, 1), quad_cnts1(1+i,2)] = pop_cnt(map(1:N/2, (N/2+1):N));
+        [quad_cnts3(1+i, 1), quad_cnts1(1+i,2)] = pop_cnt(map((N/2+1):N, 1:N/2));
+        [quad_cnts4(1+i, 1), quad_cnts1(1+i,2)] = pop_cnt(map((N/2+1):N,(N/2+1):N));
     
     % Visualize map
     col_map = mapcolour(map);
@@ -112,11 +124,44 @@ for i = 1:M
 end
 
 %close(myVideo); % close the video file
-%%
+%% Whole map
 figure
 plot(1:1:size(pop_cnts, 1), pop_cnts(:,1), 'b')
 hold on
 plot(1:1:size(pop_cnts, 1), pop_cnts(:,2), 'g')
 legend('Parasites', 'Food', 'Location', 'NorthEast')
 title(['Pop''n Dens ', num2str(pop_dens), ', fd thr ', num2str(f_th), ', para ttl ', num2str(p_ttl), ', new food ', num2str(new_food) ]);
+%% Quadrants
+figure
 
+% Quadrant 1
+subplot(2,2,1)
+plot(1:1:size(quad_cnts1, 1), quad_cnts1(:,1), 'b')
+hold on
+plot(1:1:size(quad_cnts1, 1), quad_cnts1(:,2), 'g')
+legend('Parasites', 'Food', 'Location', 'NorthEast')
+title('Quadrant 1')
+
+% Quadrant 2
+subplot(2,2,2)
+plot(1:1:size(quad_cnts2, 1), quad_cnts2(:,1), 'b')
+hold on
+plot(1:1:size(quad_cnts2, 1), quad_cnts2(:,2), 'g')
+legend('Parasites', 'Food', 'Location', 'NorthEast')
+title('Quadrant 2')
+
+% Quadrant 3
+subplot(2,2,3)
+plot(1:1:size(quad_cnts3, 1), quad_cnts3(:,1), 'b')
+hold on
+plot(1:1:size(quad_cnts3, 1), quad_cnts3(:,2), 'g')
+legend('Parasites', 'Food', 'Location', 'NorthEast')
+title('Quadrant 3')
+
+% Quadrant 4
+subplot(2,2,4)
+plot(1:1:size(quad_cnts4, 1), quad_cnts4(:,1), 'b')
+hold on
+plot(1:1:size(quad_cnts4, 1), quad_cnts4(:,2), 'g')
+legend('Parasites', 'Food', 'Location', 'NorthEast')
+title('Quadrant 4')
