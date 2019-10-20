fee8af684c37323f12cb623709a1f4fdc14f845c
richorelaxo <34532974+richorelaxo@users.noreply.github.com>
2019-10-20 12:33:47 +1000

    Add files via upload

--- Task 4/ParamSweep.m
+++ Task 4/ParamSweep.m
@@ -10,3 +10,10 @@ new_food = [100; 200; 300; 400];         % 100, 200, 300, 400
 viz = false;        % show map 
 vid = false;        % make video
 
+j = 1;
+
+for i = 1:4
+    [pop_cnts(:, j:j+1), ~, ~, ~, ~] = ...
+    para_simulation(M, N, pop_dens(i), food_growth(1), f_th(1), p_ttl(1), new_food(1), viz, vid);
+    j = j+1;
+end
